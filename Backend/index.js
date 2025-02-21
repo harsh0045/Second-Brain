adc6ce1d89d1fdf60d765bf906202a1b3362c245
Harsh <harshkesharwani53@gmail.com>
2025-02-21 22:04:51 +0530

    Fixing vercel thing

--- Backend/index.js
+++ Backend/index.js
@@ -15,13 +15,30 @@ connectDB();
 // Initialize Express app
 const app = express();
 app.use(express.json());
-app.use(cors());
+
+// Configure CORS
+const corsOptions = {
+    origin: process.env.FRONTEND_URL, // Allow specific frontend URL
+    methods: ["GET", "POST", "PUT", "DELETE"], // Allowed methods
+    allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
+  };
+  
+  
+
+const port = process.env.PORT || 3000;
+app.use(cors(corsOptions));
 
 
+app.get("/",(req,res)=>{
+    res.send("Welcome to the Second Brain API")
+});
 
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/contents", contentRouter);
 app.use("/api/v1/links", linkRouter);
 
+app.listen(port,()=>{
+    console.log(`Server is listening on ${port}`)
+})
 
 export default app;
